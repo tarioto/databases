355599345f21925918dc6c404f510539ba0fe6a0
tarioto <timarioto@gmail.com>
2016-10-07 21:56:04 -0700

    Start building users post model, rethink mental model and use rows argument in query callback

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,16 +1,19 @@
 var models = require('../models');
+var Promise = require('bluebird');
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      // somehow extract which room the get request was called from
-      var room; // req.body
-      // call models.messages.get()
-      var data = models.messages.get(room);
-      // attach to res
-      res.end(JSON.stringify(data));
-      // return res
-      return res;
+      // open a promise
+      return new Promise(function(resolve, reject) {
+        return models.messages.get(req.body) // arg to get is the chat room name
+          .then(function(data) {
+            resolve(data);
+          });
+      })
+      .then(function(data) {
+        res.send(JSON.stringify(data));
+      });
     }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
--- server/models/index.js
+++ server/models/index.js
@@ -5,20 +5,32 @@ module.exports = {
   messages: {
     get: function (room) {
       // open a promise
-      // intitialize query
-      var query = '';
-      // make our mysql query
-      if (room !== 'lobby') {
-        select = 'SELECT * FROM messages INNER JOIN rooms';
-        on = 'ON rooms.name = "' + room + '" AND rooms.id = messages.room_id';
-        
-        query = select + on;
-      } else {
-        query = 'SELECT * FROM messages';
-      }
-      // return the data
-      db.query(query, function(err, data) {
+      return new Promise(function(resolve, reject) {
+        // intitialize query
+        var query = '';
 
+        // make our mysql query
+        if (room !== 'lobby') {
+          var select = 'SELECT * FROM messages INNER JOIN rooms';
+          var on = 'ON rooms.name = "' + room + '" AND rooms.id = messages.room_id;';
+          
+          query = select + on;
+        } else {
+          query = 'SELECT * FROM messages;';
+        }
+
+        // return the data
+        db.query(query, function(err, data) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        });
+
+      })
+      .then(function(data) {
+        return data;
       });
     }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
@@ -26,8 +38,30 @@ module.exports = {
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (user) {},
+    post: function (reqData) {
+      // open a promise
+      return new Promise(function(resolve, reject) {
+
+        // check if user exists in users table
+        module.exports.users.get(reqData.user)
+          .then(function(data) {
+            var query = '';
+
+            
+          })
+          .catch(function(err) {
+            var query = '';
+
+            var insert = 'INSERT INTO users (name)';
+            var values = 'VALUES (' + reqData.name + ');';
+
+            db.query(query, function(err, data) {});
+          });
+
+      })
+      .then(function(data) {});
+    }
   }
 };
 
